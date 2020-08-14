155d66cf1e40794f4e6b2843e4496f3e2312e45b
Ruchir Dixit <ruchirtd96@gmail.com>
2020-08-14 10:26:58 +0530

    Pushing to master

---
+++ Charge.java
@@ -0,0 +1,31 @@
+public class Charge
+{
+	private final double rx,ry,q;
+	public Charge(double x0,double y0,double z0)
+	{
+		rx=x0;
+		ry=y0;
+		q=z0;
+	}
+	public double potentialAt(double x,double y)
+	{
+		double k=8.99e09;
+		double dx=x-rx;
+		double dy=y-ry;
+		return k * q / Math.sqrt(dx*dx + dy*dy);
+	}
+	public String toString()
+	{
+		return q+" at "+"("+rx+", "+ry+")";
+	}
+	public static void main(String[] args)
+	{
+		double x=Double.parseDouble(args[0]);
+		double y=Double.parseDouble(args[1]);
+		Charge c1=new Charge(0.51,0.63,21.3);
+		Charge c2=new Charge(0.13,0.94,81.9);
+		double v1=c1.potentialAt(x,y);
+		double v2=c2.potentialAt(x,y);
+		System.out.println(v1+v2);
+	}
+}
